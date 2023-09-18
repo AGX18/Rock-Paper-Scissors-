f3edc7e79b2500c3ef6215f9ab599d4f8cd715c3
Omnia Fathy <agxnh2020@gmail.com>
2023-09-18 13:12:34 +0300

    Stage 3 Done Handled invalid Input now the Game continues until user inputs "!exit"

--- src/rockpaperscissors/Main.java
+++ src/rockpaperscissors/Main.java
@@ -6,19 +6,32 @@ public class Main {
      public enum Choice {
         ROCK,
         PAPER,
-        SCISSORS
+        SCISSORS,
+         EXIT
+
+
     }
     public static void main(String[] args) {
         // put your code here
         Scanner scanner = new Scanner(System.in);
-        String userChoice = scanner.nextLine();
 
-        Choice user = Choice.valueOf(userChoice.toUpperCase());
-        Choice computer = ComputerChoice();
+        while(true) {
+            String userChoice = scanner.nextLine();
+            Choice user = userChoice(userChoice);
+            if(user == null) { // invalid input
+                continue;
+            }
+            else if (user == Choice.EXIT) {
+                return;
+            }
+            Choice computer = computerChoice();
+            String result = Result(user, computer);
+            System.out.println(result);
+        }
 
 
-        String result = Result(user, computer);
-        System.out.println(result);
+
+
 
     }
     public static String Result(Choice user, Choice computer) {
@@ -34,7 +47,29 @@ public class Main {
             return "Sorry, but the computer chose " + computer;
         }
     }
-    public static Choice ComputerChoice() {
+
+    public static Choice userChoice(String userChoice) {
+        switch (userChoice) {
+            case "rock" -> {
+                return Choice.ROCK;
+            }
+            case "paper" -> {
+                return Choice.PAPER;
+            }
+            case "scissors" -> {
+                return Choice.SCISSORS;
+            }
+            case "!exit" -> {
+                System.out.println("Bye!");
+                return Choice.EXIT;
+            }
+            default -> System.out.println("Invalid input");
+
+        }
+        return null;
+    }
+
+    public static Choice computerChoice() {
         Random random = new Random();
         int choice = random.nextInt(3);
         switch (choice) {
@@ -50,6 +85,9 @@ public class Main {
         }
         return null;
     }
+
+
+
 }
 
 
