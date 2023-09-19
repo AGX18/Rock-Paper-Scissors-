a986adf8a9e4d52f590d1469bd1ec3d6ae6a4bde
Omnia Fathy <agxnh2020@gmail.com>
2023-09-19 10:29:08 +0300

    Stage 4 Done Intoducing Score Board and Users can now view their Ratings Their ratings are stored now in rating.txt

--- src/rockpaperscissors/Main.java
+++ src/rockpaperscissors/Main.java
@@ -2,53 +2,87 @@ package rockpaperscissors;
 
 import java.util.Scanner;
 import java.util.Random;
+import java.io.File;
+import java.io.IOException;
+
+
 public class Main {
      public enum Choice {
         ROCK,
         PAPER,
         SCISSORS,
-         EXIT
+         EXIT,
+         RATING
 
 
     }
     public static void main(String[] args) {
         // put your code here
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name);
+        String filePath = "rating.txt"; // Replace with your file path
+        int score = getScore(filePath, name);
+
 
         while(true) {
             String userChoice = scanner.nextLine();
-            Choice user = userChoice(userChoice);
-            if(user == null) { // invalid input
+            Choice user = userChoice(userChoice, score);
+            if(user == null || user == Choice.RATING) { // invalid input
                 continue;
             }
-            else if (user == Choice.EXIT) {
+            else if (user == Choice.EXIT){
                 return;
             }
             Choice computer = computerChoice();
-            String result = Result(user, computer);
-            System.out.println(result);
+            score = Result(user, computer,score);
         }
 
+     }
 
 
 
-
+    private static int getScore(String filePath, String name) {
+         int score = 0;
+        try (Scanner scores = new Scanner(new File(filePath))) {
+            while (scores.hasNextLine()) {
+                String line = scores.nextLine();
+                String[] parts = line.split(" ");
+                if(name.equals(parts[0])) {
+                    score = Integer.parseInt(parts[1]);
+                    return score;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage()); // Handle any IOException that may occur
+        }
+        return score;
     }
-    public static String Result(Choice user, Choice computer) {
+
+
+    public static int Result(Choice user, Choice computer, int score) {
         if (user == computer) {
-            return "There is a draw (%s)".formatted(computer.toString().toLowerCase());
+            score += 50;
+            System.out.println("There is a draw (%s)".formatted(computer.toString().toLowerCase()));
         } else if (user == Choice.ROCK && computer == Choice.SCISSORS) {
-            return "Well done. The computer chose scissors and failed";
+            score += 100;
+            System.out.println("Well done. The computer chose scissors and failed");
         } else if (user == Choice.PAPER && computer == Choice.ROCK) {
-            return "Well done. The computer chose rock and failed";
+            score += 100;
+            System.out.println("Well done. The computer chose rock and failed");
         } else if (user == Choice.SCISSORS && computer == Choice.PAPER) {
-            return "Well done. The computer chose paper and failed";
+            score += 100;
+            System.out.println("Well done. The computer chose paper and failed");
         } else {
-            return "Sorry, but the computer chose " + computer;
+            System.out.println("Sorry, but the computer chose " + computer);
         }
+        return score;
     }
 
-    public static Choice userChoice(String userChoice) {
+
+
+    public static Choice userChoice(String userChoice, int score) {
         switch (userChoice) {
             case "rock" -> {
                 return Choice.ROCK;
@@ -62,6 +96,10 @@ public class Main {
             case "!exit" -> {
                 System.out.println("Bye!");
                 return Choice.EXIT;
+            }
+            case "!rating" -> {
+                System.out.println("Your rating: " + score);
+                return Choice.RATING;
             }
             default -> System.out.println("Invalid input");
 
